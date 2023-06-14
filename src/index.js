8d436f5a7a5e405f16b6f555b77b5c9e1a7159d7
Jamie Landsman <jamie@userscape.com>
2023-06-14 12:56:03 -0400

    get dropdown working

--- src/index.js
+++ src/index.js
@@ -1,58 +1,93 @@
 function getNy() {
   let newYorkElement = document.querySelector("#newYork");
+  if (newYorkElement) {
+    let newYorkDateElement = document.querySelector("#nYDate");
 
-  let newYorkDateElement = document.querySelector("#nYDate");
+    let newYorkTimeElement = document.querySelector("#nYTime");
 
-  let newYorkTimeElement = document.querySelector("#nYTime");
+    let nYTime = moment().tz("America/New_York");
 
-  let nYTime = moment().tz("America/New_York");
+    newYorkElement.innerHTML = "New York 🇺🇸";
 
-  newYorkElement.innerHTML = "New York 🇺🇸";
+    newYorkDateElement.innerHTML = nYTime.format("dddd, MMMM DD, YYYY");
 
-  newYorkDateElement.innerHTML = nYTime.format("dddd, MMMM DD, YYYY");
-
-  newYorkTimeElement.innerHTML = nYTime.format("h:mm:ss A");
+    newYorkTimeElement.innerHTML = nYTime.format("h:mm:ss A");
+  }
 }
 
 function getLa() {
   let laElement = document.querySelector("#lA");
+  if (laElement) {
+    let laDateElement = document.querySelector("#laDate");
 
-  let laDateElement = document.querySelector("#laDate");
+    let laTimeElement = document.querySelector("#laTime");
 
-  let laTimeElement = document.querySelector("#laTime");
+    let laTime = moment().tz("America/Los_Angeles");
 
-  let laTime = moment().tz("America/Los_Angeles");
+    laElement.innerHTML = "Los Angeles 🇺🇸";
 
-  laElement.innerHTML = "Los Angeles 🇺🇸";
+    laDateElement.innerHTML = laTime.format("dddd, MMMM DD, YYYY");
 
-  laDateElement.innerHTML = laTime.format("dddd, MMMM DD, YYYY");
-
-  laTimeElement.innerHTML = laTime.format("h:mm:ss A");
+    laTimeElement.innerHTML = laTime.format("h:mm:ss A");
+  }
 }
 
 function getTokyo() {
   let tokyoElement = document.querySelector("#tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = document.querySelector("#tokyoDate");
 
-  let tokyoDateElement = document.querySelector("#tokyoDate");
+    let tokyoTimeElement = document.querySelector("#tokyoTime");
 
-  let tokyoTimeElement = document.querySelector("#tokyoTime");
+    let tokyoTime = moment().tz("Asia/Tokyo");
 
-  let tokyoTime = moment().tz("Asia/Tokyo");
+    tokyoElement.innerHTML = "Tokyo 🇯🇵";
 
-  tokyoElement.innerHTML = "Tokyo 🇯🇵";
+    tokyoDateElement.innerHTML = tokyoTime.format("dddd, MMMM DD, YYYY");
 
-  tokyoDateElement.innerHTML = tokyoTime.format("dddd, MMMM DD, YYYY");
-
-  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss A");
-}
-
-function updateTime() {
-  setInterval(getLa, 1000);
-  setInterval(getNy, 1000);
-  setInterval(getTokyo, 1000);
+    tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss A");
+  }
 }
 
 getTokyo();
 getLa();
 getNy();
 updateTime();
+
+let selectedCity = null;
+
+function updateCity(event) {
+  selectedCity = event.target.value;
+}
+
+function updateSelectedCity() {
+  if (!selectedCity) return;
+
+  let cityName = selectedCity.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(selectedCity);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div id="cities">
+          <div class="row cityDisplay">
+            <div class="col city">${cityName}</div>
+            <div class="col time">${cityTime.format("h:mm:ss A")}</div>
+            <span class="date">${cityTime.format("dddd, MMMM DD, YYYY")}</span>
+          </div>
+    </div>
+  `;
+}
+
+function selectDropDown() {
+  let citySelectElement = document.querySelector("#citySelector");
+
+  citySelectElement.addEventListener("change", updateCity);
+}
+
+function updateTime() {
+  setInterval(getLa, 1000);
+  setInterval(getNy, 1000);
+  setInterval(getTokyo, 1000);
+  setInterval(updateSelectedCity, 1000);
+}
+
+selectDropDown();
